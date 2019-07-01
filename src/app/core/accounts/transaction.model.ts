99b93f50888e76229d8c685b0fb79d65487a5acd
philchabot <pchabot@lssinc.com>
2019-07-01 12:15:09 -0500

    Update Navigation Banner

---
+++ src/app/core/accounts/transaction.model.ts
@@ -0,0 +1,11 @@
+
+export class Transaction {
+  transactionId: string;
+  accountId: string;
+  accountName: string;
+  date: string;
+  type: string;
+  description: string;
+  amount: string;
+  balance: string;
+}
