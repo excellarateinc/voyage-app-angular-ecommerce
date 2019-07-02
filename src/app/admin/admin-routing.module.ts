6c9dd6858edb3fe16a6bb8c002e151c1f7c81117
sweber704 <sweber@lssinc.com>
2019-07-02 15:59:17 -0500

    Initial component and wiring

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UserAdminComponent } from './user-admin/user-admin.component';
+import { InviteComponent } from './invite/invite.component';
 
 const adminRoutes: Routes = [
-  { path: 'users', component: UserAdminComponent }
+  { path: 'users', component: UserAdminComponent },
+  { path: 'invite', component: InviteComponent },
 ];
 
 @NgModule({
