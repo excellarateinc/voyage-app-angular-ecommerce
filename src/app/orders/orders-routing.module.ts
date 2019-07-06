085005d28c7fc36c13c3d864e02a0fda095e8ed5
Jon Raymer <jon.raymer@lssinc.com>
2019-07-06 14:18:53 -0500

    Updates

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './orders.component';
+
+const ordersRoutes: Routes = [
+  { path: '', component: OrdersComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(ordersRoutes)
   ],
-  declarations: []
+  declarations: [],
+  exports: [RouterModule]
 })
 export class OrdersRoutingModule { }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OrdersComponent } from './orders/orders.component';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { OrdersRoutingModule } from './orders-routing.module';
+import { OrdersComponent } from './orders.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    OrdersRoutingModule,
+    AngularMaterialModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   declarations: [OrdersComponent]
 })
