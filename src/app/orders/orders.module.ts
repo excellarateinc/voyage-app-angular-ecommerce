42e2a86896711e1295a240fdb1004b7c8e6a1e28
Jon Raymer <jon.raymer@lssinc.com>
2019-07-06 14:04:01 -0500

    scaffolding

---
+++ src/app/orders/orders.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrdersComponent } from './orders/orders.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [OrdersComponent]
+})
+export class OrdersModule { }
