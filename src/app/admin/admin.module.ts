33f70f473e6c5e5341158a5c36d8671833caac3c
Jon Raymer <jon.raymer@lighthousesoftware.com>
2019-07-08 12:04:06 -0500

    updates

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material';
 import { AdminRoutingModule } from './admin-routing.module';
 import { UserAdminComponent } from './user-admin/user-admin.component';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
@@ -23,7 +22,6 @@ import { InviteService } from './invite/invite.service';
     InviteComponent
   ],
   providers: [
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
     InviteService
   ]
 
