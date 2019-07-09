dc04fdd5c76d6e34680c095040b06f9aa9a29041
Jon Raymer <jon.raymer@lighthousesoftware.com>
2019-07-09 09:00:22 -0500

    scaffolding

---
+++ src/app/authentication/linked-in-sync/linked-in-sync.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, OnDestroy, Inject, AfterViewInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { MobileService } from '../../core/mobile.service';
+import { environment } from 'environments/environment';
+import { ActivatedRoute } from '@angular/router';
+import { NotificationService } from 'app/shared/services/notification.service';
+
+@Component({
+  selector: 'app-linked-in-sync',
+  templateUrl: './linked-in-sync.component.html',
+  styleUrls: ['./linked-in-sync.component.scss']
+})
+export class LinkedInSyncComponent implements OnInit, OnDestroy {
+  codeForm: FormGroup;
+  isMobile = false;
+  working = false;
+  private watcher: Subscription;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    @Inject('Window') private window: any,
+    private mobileService: MobileService,
+    private notificationService: NotificationService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.isMobile = this.mobileService.isMobile();
+    this.watcher = this.mobileService.mobileChanged$.subscribe((isMobile: boolean) => {
+      this.isMobile = isMobile;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.watcher.unsubscribe();
+  }
+
+  syncWithLinkedIn(): void {
+    if (this.codeForm.invalid) {
+      return;
+    }
+    this.working = true;
+    const code = this.codeForm.value.code;
+    this.window.location.href = `${environment.SERVER_URL}/Authentication/LoginLink?token=${code}&isMobile=true`;
+  }
+
+  private initializeForm(): void {
+    this.codeForm = this.formBuilder.group({
+      code: ['', Validators.required]
+    });
+  }
+}
